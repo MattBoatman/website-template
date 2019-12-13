0bc696f2ff650db0adc31d4a18c5e46b1ef13d78
Matt Boatman <matthewd.boatman@gmail.com>
2019-12-12 22:56:03 -0500

    switch layout

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -11,6 +11,21 @@ const Layout = () => {
     <div>
       <InitialView />
       <Section
+        backgroundColor="#31708E"
+        color="#f7f9fb"
+        title="About Kimberly"
+        paragraphs={[
+          "Kimberly is a licensed massage therapist in the state of Texas (TX license: MT#005028). She has over 25 years of professional experience. She is on call 24 hours a day, so call at anytime and Kimberly will make your stress a thing of the past!"
+        ]}
+        imageSrc={kim}
+        id="aboutKimberly"
+        buttonText="Services"
+        scrollId="services"
+        flipPhoto
+        altText="About"
+        addPhoneNumber
+      />
+      <Section
         backgroundColor="#f7f9fb"
         color="#000"
         title="DFW Dallas Outcall Massage"
@@ -23,21 +38,6 @@ const Layout = () => {
         buttonText="About Kimberly"
         scrollId="aboutKimberly"
         altText="Dallas"
-        addPhoneNumber
-      />
-      <Section
-        backgroundColor="#31708E"
-        color="#f7f9fb"
-        title="About Kimberly"
-        paragraphs={[
-          "Kimberly is a licensed massage therapist in the state of Texas (TX license: MT#005028). She has over 25 years of professional experience. She is on call 24 hours a day, so call at anytime and Kimberly will make your stress a thing of the past!",
-        ]}
-        imageSrc={kim}
-        id="aboutKimberly"
-        buttonText="Services"
-        scrollId="services"
-        flipPhoto
-        altText="About"
         addPhoneNumber
       />
       <Section
